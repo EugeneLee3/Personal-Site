f3ee2326f511ca21e7f64ad9163743e6fb7cc01e
Genyuel <114523960+EugeneLee3@users.noreply.github.com>
2023-01-14 16:33:02 -0500

    playing around with backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
+// const {PythonShell} = require('python-shell');
+
+// let options = {
+//     scriptPath: "./script",
+//     args: ["AAPL", 45]
+// }
+
+// PythonShell.run("test.py", options, (err,res) => {
+//     if (err) console.log(err)
+//     if (res) console.log(res)
+// })
+
+
 const express = require('express');
 const app = express();
+const mysql = require('mysql')
 
-//https://www.youtube.com/watch?v=3YrOOia3-mo
+const db = mysql.createPool({
+    host: 'localhost', 
+    user: 'root',
+    password: 'password',
+    database: 'PersonalSiteDatabase'
+})
+
 
 app.get('/', (req, res) => {
-    res.send("hlod");
+    const sqlInsert = "INSERT INTO blog_comments (blogPost, blogComment) VALUES ('test', 'test v2');";
+    db.query(sqlInsert, (err, result) => {
+        res.send("test 123");
+    });
+    console.log('bye');
 });
 
 app.listen(3001, () => {
