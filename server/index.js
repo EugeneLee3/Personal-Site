1487536858a9c8f7b9b856f7264abdb7a95fd58c
Genyuel <114523960+EugeneLee3@users.noreply.github.com>
2023-02-02 17:47:53 -0500

    test

--- server/index.js
+++ server/index.js
@@ -1,36 +1,20 @@
-// const {PythonShell} = require('python-shell');
-
-// let options = {
-//     scriptPath: "./script",
-//     args: ["AAPL", 45]
-// }
-
-// PythonShell.run("test.py", options, (err,res) => {
-//     if (err) console.log(err)
-//     if (res) console.log(res)
-// })
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql')
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
 
-const db = mysql.createPool({
-    host: 'localhost', 
-    user: 'root',
-    password: 'password',
-    database: 'PersonalSiteDatabase'
-})
-
-
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO blog_comments (blogPost, blogComment) VALUES ('test', 'test v2');";
-    db.query(sqlInsert, (err, result) => {
-        res.send("test 123");
-    });
-    console.log('bye');
-});
-
-app.listen(3001, () => {
-    console.log("running on port 3001");
+// driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // database connection
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });
